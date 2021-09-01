dc59907b2d7f3521b1501c58c89611a2a6894cc6
henry walton <hnrywltn@gmail.com>
2021-09-01 11:55:52 -0700

    styled splash page

--- react-app/src/components/Splash/Splash.js
+++ react-app/src/components/Splash/Splash.js
@@ -21,22 +21,19 @@ function Splash() {
   return (
     <div className="splashpage">
       <h1 className="title">pro_docs</h1>
-      <div className="about">
-        <p>
-          pro_docs is an app that can help you stay organized while planning a project.
-        </p>
-        <p>
-          choose technologies, build your user stories, database schemas, and get documentation including readmes'.
-        </p>
+      <div className="splashabout">
+          pro_docs is an app that can help you track your projects in a user friendly way.
+          choose technologies, build your resource collection, and find others on gitHub.
+
       </div>
       <div className="icon-container">
         <img src="https://i.imgur.com/ypzl1sH.png" alt="icon"/>
       </div>
-      <Link to="/login">
+      <Link className="splashLogin" to="/login">
         <img src="https://i.imgur.com/xftZfBi.png" alt="login"/>
       </Link>
-      <Link to="/sign-up">
-        <p>SignUp</p>
+      <Link className="splashSignup" to="/sign-up">
+        SignUp
       </Link>
       <button
         className="demobttn"
@@ -44,10 +41,10 @@ function Splash() {
       >
         DEMO
       </button>
-      <a href="https://www.linkedin.com/in/henry-walton-324977187/" target="_blank">
+      <a href="https://www.linkedin.com/in/henry-walton-324977187/" target="_blank" rel="noreferrer" className="splashLinkden">
         <img src="https://i.imgur.com/yHgUB15.png" alt="login"/>
       </a>
-      <a href="https://github.com/hnrywltn" target="_blank">
+      <a href="https://github.com/hnrywltn" target="_blank" rel="noreferrer" className="splashgithub">
         <img src="https://i.imgur.com/35z8Aim.png" alt="login"/>
       </a>
     </div>
